10ba3993376941488b3116102a7a964f138b252f
saulorodriguesm <saulorodm@gmail.com>
2024-06-05 21:41:07 -0300

    change: order changed on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,21 +16,26 @@ const Footer = () => {
           <img className="w-32 mb-5" src={Assets.whiteLogo} />
           <div className=" text-white my-3">πping projetos </div>
           <div className=" text-white my-3">CNPJ: 55.308.283/0001-25 </div>
-          <div className="flex my-3">
-            <img className="w-6 mr-3" src={Assets.instagram} />
-            <img className="w-6" src={Assets.linkedin} />
-          </div>
-          <div className="my-3 text-white">paulo.silva@pi-ping.com </div>
           <div className="my-3 text-white">
             Horário de atendimento Seg. à Qui. das 08:00h às 18:00h | Sex. das
             08:00h às 17:00h{" "}
           </div>{" "}
         </div>
-        <div className="flex items-end text-white text-xs">
-          desenvolvido por{" "}
-          <a target="_blank" href="https://www.linkedin.com/in/saulorodriguesm">
-            : Saulo Rodrigues
-          </a>
+        <div className="flex flex-col justify-between text-white text-xs">
+          <div className="my-3 text-white">paulo.silva@pi-ping.com </div>
+          <div className="flex my-3">
+            <img className="w-6 mr-3" src={Assets.instagram} />
+            <img className="w-6" src={Assets.linkedin} />
+          </div>
+          <div className="text-white">
+            desenvolvido por{" "}
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/saulorodriguesm"
+            >
+              : Saulo Rodrigues
+            </a>{" "}
+          </div>
         </div>
       </div>
     </div>
